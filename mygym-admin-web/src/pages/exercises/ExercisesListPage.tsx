b6e7af5b0fc872c45dfe7b911a3766174587a18a
christoforosl <christoforosl08@gmail.com>
2021-01-25 18:56:06 +0200

    temp commit

--- mygym-admin-web/src/pages/exercises/ExercisesListPage.tsx
+++ mygym-admin-web/src/pages/exercises/ExercisesListPage.tsx
@@ -3,9 +3,17 @@ import config from "../../globals/AppVars.json"
 import { apiResults } from "../../nufR/APICallerUtils"
 import ReactDataGrid from 'react-data-grid';
 import { Fragment } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const axios = require('axios').default;
+
+interface IExercise {
+    exerciseDefinitionId: number;
+    name: string;
+    description: string;
+  }
+
+const defaultProps:IExercise[] = [];
 
 const columns = [
     { key: 'exerciseDefinitionId', name: 'ID' },
@@ -13,31 +21,36 @@ const columns = [
     { key: 'description', name: 'Description' } ];
 
 export default function ExercisesListPage() {
-    
+
+    const [data, setData]: [IExercise[], (posts: IExercise[]) => void] = useState(defaultProps);
     const url:string = process.env.REACT_APP_API_ROOT+config.apiEndPoints.getExerciseList.url;
 
     useEffect(() => {
-        var data  = axios.get(url)
-        .then( (response: any) => {
-            // handle success
-            if (response.status==='ERROR') {
-                throw response.error;
-            }
-            //console.log( response.data );
-            const ret: apiResults = response.data;
-            return ret.results);
-        
-        console.log("data:" + JSON.stringify(data));
+        const fetchData = async () => {
+          const result = await axios( url );
+          console.log("data:" + JSON.stringify( result.data.results) ) ;
+          setData(result.data.results);
+        };
+     
+        fetchData();
+    }, []);
+    
+    // return (
+    // <ul>
+    //     {data.map(item => (
+    //       <li key={item.exerciseDefinitionId}>
+    //         {item.name}
+    //       </li>
+    //     ))}
+    //   </ul>);
 
-    },[]));
-
+    
     return (
-        data ? 
+        data.length > 0 ? 
         <ReactDataGrid
                 columns={columns}
-                rowGetter={i => data[i]}
-                rowsCount={10}
+                rows={data}
                 minHeight={150} /> : <Fragment>Loading...</Fragment>);
-    
-    
+
+            
 }
